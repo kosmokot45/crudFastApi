5784fda1e84da53aaac7a835478340ed75b40554
kosmokot45 <kosmokot45@gmail.com>
2023-07-03 16:01:22 +0300

    Update resp.py

--- resp.py
+++ resp.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 from fastapi.responses import PlainTextResponse, JSONResponse, HTMLResponse
 
 app = FastAPI()
@@ -21,3 +21,21 @@ def get_json():
 @app.get("/html", response_class=HTMLResponse)
 def read_root():
     return "<h2>Hello FastApi</h2>"
+
+@app.get("/user/admin")
+def user_admin():
+    return("Admin") 
+
+@app.get("/user/{name}/{age}/{id}")
+def users(name: str = Path(min_length=2, max_length=20),
+          age: int = Path(ge=18, lt=111),
+          id: int = Path(ge=1)):
+    return {"name":name,"age":age,"id":id}
+
+@app.get("/users")
+def get_model(name: str = "undef", age: int = 18):
+    return {"user_name": name, "user_age": age}
+
+@app.get("/userss")
+def get_model(name, age: int = Query(ge=18)):
+    return {"user_name": name, "user_age": age}
